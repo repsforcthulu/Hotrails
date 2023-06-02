e8a6de3b6c754be89ff543d117152fa2acd21ec6
Brett Evans <evansbrett86@gmail.com>
2023-06-01 20:22:50 -0400

    ch 2

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
+  #switch to non headless to watch it vroom
+  driven_by :selenium, using: :headless_firefox, screen_size: [1400, 1400]
 end
